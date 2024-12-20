beccebd6c05788b3943d68649a4d36540d716ffd
newbie060 <poozagrg432@gmail.com>
2024-12-20 19:32:43 +0545

    meme generator

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './Header'
+import Meme from './Meme'
+import './App.css'
+
+function App() {
+  
+  return (
+    <div>
+      <Header />
+      <Meme />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+
+function Header() {
+    return (
+        <header>
+            <img className="header--image" src=".\images\troll-face.png"/>
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--des">Just for fun</h4>
+        </header>
+    )
+}
+
+export default Header
